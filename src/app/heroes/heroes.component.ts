aba0e939ef7ccb3b503e289d2afa7e96dde27095
Claudia <clbojaca@sistecredito.com>
2021-07-23 09:27:40 -0500

    adding navigation among different components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,7 +13,6 @@ import { MessageService } from '../services/message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor( private heroService: HeroService, private messageService: MessageService ) { }
 
@@ -26,10 +25,5 @@ export class HeroesComponent implements OnInit {
       heroes => this.heroes = heroes
     );
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
+ 
 }
